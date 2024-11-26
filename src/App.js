69e051e655a5c46937742d9787d852f1021d188c
siers22 <siers6292@gmail.com>
2024-11-27 02:17:23 +0500

    хуня

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
     return (
         <div className="app-wrapper">
             <Header></Header>
             <Nav> </Nav>
-            <Profile name="cheel" prof_descr="cheeeeel" ></Profile>
+            <Dialogs></Dialogs>
+            {/*<Profile name="cheel" prof_descr="cheeeeel" ></Profile>*/}
         </div>
     )
 }
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,7 @@ import c from './Dialogs.module.css';
 let Dialogs = (props) => {
     return (
         <div className={c.dialogs_wrapper}>
-
+            <p>dialogs</p>
         </div>
     )
 }
